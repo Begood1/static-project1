5d1970090f6a2d36e6d8cca3329ca18efadb7304
Begood1 <geze1010@gmail.com>
2019-03-25 15:39:43 +0700

    add images to web

--- .eleventy.js
+++ .eleventy.js
@@ -1,20 +1,49 @@
 const day = require('dayjs');
 const htmlmin = require('html-minifier');
+const t = require('./config/languages.json');
+const config = require('./config/config.json');
 
 module.exports = eleventyConfig => {
+  /**
+   * Filters
+   */
+  eleventyConfig.addFilter('t', (key, lang = config.site.lang) => t[lang][key]);
 
-  eleventyConfig.setLiquidOptions({
-    dynamicPartials: true
+  //eleventyConfig.addFilter('formatDate', date => day(date).format('MMM D, YYYY'));
+  eleventyConfig.addFilter('formatDate', (date, format = 'MMM D, YYYY') => day(date).format(format));
+  eleventyConfig.addFilter('getLang', path => {
+    const id = path.slice(2).split('/')[1];
+    return id.length === 2 ? id : config.site.lang;
   });
 
+  eleventyConfig.addFilter('getImageUrl', (url, width) => `${url}-/resize/${width}x/-/quality/lighter/`);
+
+  /**
+   * Shortcodes
+   */
   eleventyConfig.addShortcode('Icon', logo => `
     <svg aria-hidden="true" role="presentation">
       <use xlink:href="#icon-${logo}" />
     </svg>
   `);
 
-  eleventyConfig.addFilter('formatDate', date => day(date).format('MMM D, YYYY'));
+  eleventyConfig.addShortcode('Image', (url, alt = config.site.name) => `
+    <img alt="${alt}" src="${url}-/resize/800x/-/quality/lighter/" 
+    srcset="
+      ${url}-/resize/1600x/-/quality/lighter/ 1600w, 
+      ${url}-/resize/1440x/-/quality/lighter/ 1440w, 
+      ${url}-/resize/1280x/-/quality/lighter/ 1280w, 
+      ${url}-/resize/1024x/-/quality/lighter/ 1024w, 
+      ${url}-/resize/768x/-/quality/lighter/ 768w, 
+      ${url}-/resize/576x/-/quality/lighter/ 576w" >
+  `);
 
+  /**
+   * Templating config
+   */
+  eleventyConfig.setLiquidOptions({
+    dynamicPartials: true
+  });
   eleventyConfig.addTransform('htmlmin', (content, outputPath) => {
     if (outputPath.endsWith('.html')) {
       return htmlmin.minify(content, {
@@ -26,6 +55,9 @@ module.exports = eleventyConfig => {
     return content;
   });
 
+  /**
+   * Copy our assets folders as is
+   */
   eleventyConfig.addPassthroughCopy('admin');
   eleventyConfig.addPassthroughCopy('assets/dist');
   eleventyConfig.addPassthroughCopy('assets/fonts');
@@ -39,3 +71,46 @@ module.exports = eleventyConfig => {
     }
   };
 };
+
+
+// const day = require('dayjs');
+// const htmlmin = require('html-minifier');
+
+// module.exports = eleventyConfig => {
+
+//   eleventyConfig.setLiquidOptions({
+//     dynamicPartials: true
+//   });
+
+//   eleventyConfig.addShortcode('Icon', logo => `
+//     <svg aria-hidden="true" role="presentation">
+//       <use xlink:href="#icon-${logo}" />
+//     </svg>
+//   `);
+
+//   eleventyConfig.addFilter('formatDate', date => day(date).format('MMM D, YYYY'));
+
+//   eleventyConfig.addTransform('htmlmin', (content, outputPath) => {
+//     if (outputPath.endsWith('.html')) {
+//       return htmlmin.minify(content, {
+//         useShortDoctype: true,
+//         removeComments: true,
+//         collapseWhitespace: true
+//       });
+//     }
+//     return content;
+//   });
+
+//   eleventyConfig.addPassthroughCopy('admin');
+//   eleventyConfig.addPassthroughCopy('assets/dist');
+//   eleventyConfig.addPassthroughCopy('assets/fonts');
+//   eleventyConfig.addPassthroughCopy('assets/favicons');
+
+//   return {
+//     dir: {
+//       input: '.',
+//       data: 'config',
+//       includes: 'views',
+//     }
+//   };
+// };
--- assets/javascripts/app.js
+++ assets/javascripts/app.js
@@ -1,28 +1,49 @@
+import Flickity from 'flickity';
+import 'flickity/dist/flickity.min.css';
 import '../stylesheets/style.scss';
-import qs from 'qs';
-import axios from 'axios';
 import * as config from '../../config/config.json';
 
-const form = document.querySelector('.form');
-const email = document.querySelector('.email');
-const comment = document.querySelector('.comment');
-const name = document.querySelector('.name');
+const flickity = new Flickity('.gallery', {
+  contain: true,
+  // More options are available:
+  // https://flickity.metafizzy.co/options.html
+});
 
-// ðŸ‘† Write homework code here
+const burger = document.querySelector('button.burger');
+const burgerActive = 'burger--morph';
+const mobilePanel = document.querySelector('.mobile__panel');
+const mobilePanelActive = 'mobile__panel--open';
 
-const handleSubmit = e => {
-  e.preventDefault();
-  const formData = {
-    name: name.value,
-    email: email.value,
-    comment: comment.value,
-    priority: document.querySelector('input[name="priority"]:checked').value,
-    topic: [...document.querySelectorAll('input[name="topic"]:checked')].map(v => v.value),
-  };
-  axios
-    .post(config.site.url, qs.stringify({ 'form-name': config.form.name, ...formData }))
-    .then(_ => console.log('success'))
-    .catch(_ => console.log('failed'));
-};
+burger.addEventListener('click', () => {
+  burger.classList.toggle(burgerActive);
+  mobilePanel.classList.toggle(mobilePanelActive);
+});
 
-form.addEventListener('submit', handleSubmit);
+// import '../stylesheets/style.scss';
+// import qs from 'qs';
+// import axios from 'axios';
+// import * as config from '../../config/config.json';
+
+// const form = document.querySelector('.form');
+// const email = document.querySelector('.email');
+// const comment = document.querySelector('.comment');
+// const name = document.querySelector('.name');
+
+// // ðŸ‘† Write homework code here
+
+// const handleSubmit = e => {
+//   e.preventDefault();
+//   const formData = {
+//     name: name.value,
+//     email: email.value,
+//     comment: comment.value,
+//     priority: document.querySelector('input[name="priority"]:checked').value,
+//     topic: [...document.querySelectorAll('input[name="topic"]:checked')].map(v => v.value),
+//   };
+//   axios
+//     .post(config.site.url, qs.stringify({ 'form-name': config.form.name, ...formData }))
+//     .then(_ => console.log('success'))
+//     .catch(_ => console.log('failed'));
+// };
+
+// form.addEventListener('submit', handleSubmit);
